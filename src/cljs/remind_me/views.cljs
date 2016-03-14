88a49eb60c1d780231189407109c0887a3724269
not-much-io <kristo.koert@gmail.com>
2016-03-14 08:22:12 +0200

    rtying_out_react-motion

--- src/cljs/remind_me/views.cljs
+++ src/cljs/remind_me/views.cljs
@@ -1,7 +1,8 @@
 (ns remind-me.views
   (:require [re-frame.core :as re-frame]
             [goog.dom :as gdom]
-            [ajax.core :refer [POST]]))
+            [ajax.core :refer [POST]]
+            [remind-me.motion :as motion]))
 
 (defn add-form []
   (let [get-reminder-data #(.-value (gdom/getElement "add-reminder-input"))
@@ -20,24 +21,33 @@
                         (get-reminder-data))
                       (clear-input))} "Add"]]))
 
-(def ctg (reagent.core/adapt-react-class js/React.addons.CSSTransitionGroup))
-
 (defn reminders-list [reminders]
   (let [set-cls-inactive (fn [id]
                            (set! (.-className
                                    (gdom/getElement id))
-                                 "removing"))]
+                                 "removing"))
+        ids              (map :id @reminders)
+        will-leave       (fn []
+                           {:width  (motion/spring 0)
+                            :height (motion/spring 0)})]
     [:div
-     [:ul#reminder-list
-      [ctg {:transition-name "foo"}
-       (for [{:keys [id name]} @reminders]
-         (let [el-id (str "liid" id)]
-           ^{:key id} [:li {:id       el-id
-                            :on-click #(do
-                                        (set-cls-inactive el-id)
-                                        (re-frame/dispatch [:remove-reminder id]))}
-                       name]))]
-      ]]))
+     [motion/TransitionMotion
+      {:willLeave will-leave
+       :styles    (map (fn [id]
+                         {:key   id
+                          :style {:height (motion/spring 10)
+                                  :width  (motion/spring 10)}}) ids)}
+      (fn []
+        [:ul#reminder-list
+         (fn []
+           (for [{:keys [id name]} @reminders]
+             (let [el-id (str "li-id" id)]
+               ^{:key id} [:li {:id       el-id
+                                :on-click #(do
+                                            (set-cls-inactive el-id)
+                                            (re-frame/dispatch [:remove-reminder id]))}
+                           name]))
+           )])]]))
 
 (defn main-panel []
   (let [reminders (re-frame/subscribe [:reminders])]
